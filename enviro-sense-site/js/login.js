afd73ac6b0a102db965b62bc635869cb8f2f7064
Bruno Araujo <bruno.maia@sptech.school>
2024-10-16 16:35:27 -0300

    Atualização CSS tela login

--- enviro-sense-site/js/login.js
+++ enviro-sense-site/js/login.js
@@ -13,58 +13,73 @@ function mostrarSenha() {
 
 function mostrarCaptcha() {
 
-    var imagemSorteada = 0
-
     
-        chk_robo.hidden = true
-        btn_entrar.hidden = true
-        div_campo_captcha.innerHTML = ""
-        imagens.innerHTML = `Escolha a imagem que contém um círculo<br>`
-        for (var imagem = 1;imagem<=9;imagem++) {
-            imagemSorteada = parseInt(Math.random()*9+1)
-            if (imagemSorteada == 1) {
-                imagens.innerHTML += `<img src="images/captcha/${imagemSorteada}.png" style="height: 50px; width: 50px;" onclick ="verificarRobo()">`
-                }else {
-                imagens.innerHTML += `<img src="images/captcha/${imagemSorteada}.png" style="height: 50px; width: 50px;" onclick ="errarVerificacao()">`
-                }
-            if (imagem % 3 == 0) {
-                imagens.innerHTML +=`<br>`
+    var imagemCorreta = 0;
+
+    chk_robo.hidden = true;
+    btn_entrar.hidden = true;
+    div_campo_captcha.innerHTML = "";
+    imagens.innerHTML = `Escolha a imagem que contém uma plantinha<br>`;
+    for (var imagem = 1; imagem <= 9; imagem++) {
+        imagemSorteada = parseInt(Math.random() * 7 + 1);
+        if (imagemSorteada == 1) {
+            if(imagemCorreta == 0){
+                imagemCorreta++     
+            imagens.innerHTML += `<img src="images/captcha/${imagemSorteada}.png" style="height: 70px; width: 70px;" onclick ="verificarRobo()">`;
+            }else {
+                imagem--
             }
+        } else {
+            imagens.innerHTML += `<img src="images/captcha/${imagemSorteada}.png" style="height: 70px; width: 70px;" onclick ="errarVerificacao()">`;
         }
-        imagens.innerHTML += `<button class="botao" onclick="sortearCaptcha()">Pular</button>`
+        if (imagem % 3 == 0) {
+            imagens.innerHTML += `<br>`;
+        }
+    }
+    imagens.innerHTML += `<button class="botao" onclick="sortearCaptcha()">Pular</button>`;
 
 }
 function sortearCaptcha() {
-    imagens.innerHTML = `Escolha a imagem que contém um círculo<br>`
-        for (var imagem = 1;imagem<=9;imagem++) {
-            imagemSorteada = parseInt(Math.random()*9+1)
-            if (imagemSorteada == 1) {
-                imagens.innerHTML += `<img src="images/captcha/${imagemSorteada}.png" style="height: 50px; width: 50px;" onclick ="verificarRobo()">`
+
+    var imagemCorreta = 0;
+
+
+    imagens.innerHTML = `Escolha a imagem que contém uma plantinha<br>`;
+    for (var imagem = 1; imagem <= 9; imagem++) {
+        imagemSorteada = parseInt(Math.random() * 7 + 1);
+        if (imagemSorteada == 1) {
+            if(imagemCorreta == 0){
+                imagemCorreta++
+                imagens.innerHTML += `<img src="images/captcha/${imagemSorteada}.png" style="height: 70px; width: 70px;" onclick ="verificarRobo()">`;
                 }else {
-                imagens.innerHTML += `<img src="images/captcha/${imagemSorteada}.png" style="height: 50px; width: 50px;" onclick ="errarVerificacao()">`
+                    imagem--
                 }
-            if (imagem % 3 == 0) {
-                imagens.innerHTML +=`<br>`
-            }
+        } else {
+            imagens.innerHTML += `<img src="images/captcha/${imagemSorteada}.png" style="height: 70px; width: 70px;" onclick ="errarVerificacao()">`;
         }
-        imagens.innerHTML += `<button class="botao" onclick="sortearCaptcha()">Pular</button>`
+        if (imagem % 3 == 0) {
+            imagens.innerHTML += `<br>`;
+        }
+    }
+    imagens.innerHTML += `<button class="botao" onclick="sortearCaptcha()">Pular</button>`;
 
 }
 
 
 function verificarRobo() {
-    div_campo_captcha.innerHTML = `<input type="checkbox" id="chk_robo" class="campo_captcha" onclick="mostrarCaptcha()">Você não é um robô`
+    div_campo_captcha.innerHTML = `<input type="checkbox" id="chk_robo" class="campo_captcha" onclick="mostrarCaptcha()">Você não é um robô`;
     chk_robo.checked = true;
-    imagens.innerHTML = ""
-    btn_entrar.hidden = false
+    imagens.innerHTML = "";
+    btn_entrar.hidden = false;
 
 }
 
 function errarVerificacao() {
-    div_campo_captcha.innerHTML = `<input type="checkbox" id="chk_robo" class="campo_captcha" onclick="mostrarCaptcha()">Não sou um robô`
+    div_campo_captcha.innerHTML = `<input type="checkbox" id="chk_robo" class="campo_captcha" onclick="mostrarCaptcha()">Não sou um robô`;
     chk_robo.checked = false;
-    imagens.innerHTML = ""
-    btn_entrar.hidden = false
+    imagens.innerHTML = "";
+    btn_entrar.hidden = false;
+    alert("Captcha escolhido incorretamente, tente novamente!");
 
 }
 
@@ -72,7 +87,7 @@ function entrar() {
     if (chk_robo.checked != true) {
         alert("Erro no login!");
     } else {
-        alert("Seja bem vindo!")
-        window.location.href='dashboards.html'
+        alert("Seja bem vindo!");
+        window.location.href = 'dashboards.html';
     }
 }
