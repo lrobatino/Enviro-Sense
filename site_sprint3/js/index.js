98b4acfea9ae56dcec3fd76fba9e87b95d424b5b
Bruno Araujo <bruno.maia@sptech.school>
2024-11-05 21:45:27 -0300

    Atualizações no index e criação da pasta js

---
+++ site_sprint3/js/index.js
@@ -0,0 +1,13 @@
+var posicao = 0
+passarCarrosel()
+
+function passarCarrosel() {
+  var slidejs = document.getElementsByClassName("slide");
+  for (var posicaoSlide = 0; posicaoSlide < slidejs.length; posicaoSlide++) {
+    slidejs[posicaoSlide].style.display = "none"; 
+  }
+  posicao++;
+  if (posicao > slidejs.length) {posicao = 1}
+  slidejs[posicao-1].style.display = "block"; 
+  setTimeout(passarCarrosel, 2000); 
+}
