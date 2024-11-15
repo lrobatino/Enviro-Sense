d088826bdc718a7a62608880b6c86c62e23f0d69
Eduardo Almeida <eduardo.mendes@sptech.school>
2024-11-15 16:08:39 -0300

    Js para trocar de página tela de cadastro

--- site_sprint3/js/cadastro.js
+++ site_sprint3/js/cadastro.js
@@ -1,2 +1,21 @@
+window.onload = function() {
+    document.getElementById("continue-button").onclick = function() {
+        // Esconde os labels e inputs visíveis
+        document.querySelectorAll('.campo-cadastro').forEach(el => {
+            el.classList.add('hide'); // adiciona a classe 'hide' a cada elemento com a classe 'campo-cadastro'
+            el.classList.remove('campo-cadastro'); // remove a classe 'campo-cadastro' de cada elemento
+        });
 
+        // Mostra os labels e inputs que estão escondidos
+        document.querySelectorAll('.hide.escondido').forEach(el => {
+            el.classList.remove('hide');
+            el.classList.remove('escondido');
+        });
 
+        // Esconde o botão "Continuar" e mostra o botão "Cadastrar"
+        document.getElementById('continue-button').classList.add('hide');
+        document.getElementById('cadastrar').classList.remove('hide');
+
+        document.querySelector('.cadastro').style.height = "65vh";
+    };
+};
